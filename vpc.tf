12e9c086c206e38e74008f28b8c3ba8d4b598f93
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-05-28 19:39:46 -0400

    Adding Public, Private, Database subnet cidrs

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,11 @@
-# VPC roboshop-dev
+# VPC ---> roboshop-dev
 resource "aws_vpc" "main"{
     cidr_block = var.cidr_block
     instance_tenancy = "default"
     enable_dns_hostnames = "true"
 
     tags = merge(
+        var.vpc_tags,
         local.common_tags,
         {
             Name = "${var.project}-${var.environment}"
@@ -12,11 +13,12 @@ resource "aws_vpc" "main"{
     )
 }
 
-# IGW roboshop-dev
+# IGW ---> roboshop-dev
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id   # Association with VPC
 
   tags = merge(
+        var.igw_tags,
         local.common_tags,
         {
             Name = "${var.project}-${var.environment}"
@@ -24,8 +26,8 @@ resource "aws_internet_gateway" "main" {
     )
 }
 
-# Public subnets roboshop-dev-us-east-1a
-resource "aws_subnet" "main" {
+# Public subnets ---> roboshop-dev-public-us-east-1a
+resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
@@ -33,9 +35,42 @@ resource "aws_subnet" "main" {
   map_public_ip_on_launch = true
 
   tags = merge(
+        var.public_subnet_tags,
         local.common_tags,
         {
             Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        }
+    )
+}
+
+# Private subnets ---> roboshop-dev-private-us-east-1a
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        var.private_subnet_tags,
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+        }
+    )
+}
+
+# Private subnets ---> roboshop-dev-public-us-east-1a
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(
+        var.database_subnet_tags, 
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
         }
     )
 }
