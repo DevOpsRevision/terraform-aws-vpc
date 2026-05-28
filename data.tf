0c2df10b9092c1499415bd6d419f92bbdcaf56fe
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-05-28 19:01:44 -0400

    Initial Infrastructure code

---
+++ data.tf
@@ -0,0 +1,8 @@
+data "aws_availability_zones" "available"{
+    state = "available"
+}
+
+# Below is just for testing how the output looks for name formation.
+# output "azs_info" {
+#   value = data.aws_availability_zones.available
+# }
