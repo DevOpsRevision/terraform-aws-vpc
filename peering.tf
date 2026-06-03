74fd4513048a9d64b3373c6e99878324d6d57b98
naveen21kumar95 <naveen21kumar95@gmail.com>
2026-06-02 22:35:29 -0400

    Adding Peering Connection

--- peering.tf
+++ peering.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc_peering_connection" "default" {
   count =  var.is_peering_required ? 1 : 0 
-
-  
+ 
   peer_vpc_id   = data.aws_vpc.default.id   # Acceptor VPC which is default here
   vpc_id        = aws_vpc.main.id           # Requestor VPC 
 
@@ -23,4 +22,40 @@ resource "aws_vpc_peering_connection" "default" {
     }
   )
 
+}
+
+#Route -- Public 
+resource "aws_route" "public_peering" {
+  count =  var.is_peering_required ? 1 : 0
+
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+#Route -- Private 
+resource "aws_route" "private_peering" {
+  count =  var.is_peering_required ? 1 : 0
+
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+#Route -- Public 
+resource "aws_route" "database_peering" {
+  count =  var.is_peering_required ? 1 : 0
+
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+## We should add peering connection in default VPC main route table too.
+resource "aws_route" "default_peering" {
+  count =  var.is_peering_required ? 1 : 0
+
+  route_table_id            = data.aws_route_table.main.id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
